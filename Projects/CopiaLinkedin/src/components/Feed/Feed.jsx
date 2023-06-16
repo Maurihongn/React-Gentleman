d2576cc6fcd8d6086745c31c3902fe12f6b235f9
Maurihongn <mauriciohongn282@gmail.com>
2023-06-16 13:39:17 -0300

    asd

--- Projects/CopiaLinkedin/src/components/Feed/Feed.jsx
+++ Projects/CopiaLinkedin/src/components/Feed/Feed.jsx
@@ -1,34 +1,35 @@
-import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay';
-import CreateIcon from '@mui/icons-material/Create';
-import EventNoteIcon from '@mui/icons-material/EventNote';
-import ImageIcon from '@mui/icons-material/Image';
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import './Feed.css';
-import { InputOption } from './InputOption';
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+import CreateIcon from "@mui/icons-material/Create";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import "./Feed.css";
+import { InputOption } from "./InputOption";
 
 const Feed = () => {
   return (
-    <div className='Feed'>
-      <div className='feed-inputContainer'>
-        <div className='feed-input'>
+    <div className="Feed">
+      <div className="feed-inputContainer">
+        <div className="feed-input">
           <CreateIcon />
           <form>
-            <input type='text' />
-            <button type='submit'>Send</button>
+            <input type="text" />
+            <button type="submit">Send</button>
           </form>
         </div>
-        <div className='feed-inputOptions'>
+        <div className="feed-inputOptions">
           {/* Input options */}
-          <InputOption Icon={ImageIcon} title='Photo' color='#70b5f9' />
-          <InputOption Icon={SubscriptionsIcon} title='Video' color='#e7a33e' />
-          <InputOption Icon={EventNoteIcon} title='Event' color='#c0cbcd' />
+          <InputOption Icon={ImageIcon} title="Photo" color="#70b5f9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#e7a33e" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#c0cbcd" />
           <InputOption
             Icon={CalendarViewDayIcon}
-            title='Write article'
-            color='#7fc15e'
+            title="Write article"
+            color="#7fc15e"
           />
         </div>
       </div>
+      {/* Posts */}
     </div>
   );
 };
---
+++ Projects/CopiaLinkedin/src/components/Feed/Post/Post.jsx
@@ -0,0 +1,7 @@
+import "./Post.css";
+
+const Post = () => {
+  return <div className="Post">Post</div>;
+};
+
+export default Post;
